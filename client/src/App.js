0e5a7068fd1f9bce0134a4ad2f178f90648a7da0
github username <github email address>
2018-07-19 14:17:13 -0400

    Deleted some bloat, starting protected routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import TestLoginButton from './components/TestLoginButton';
+import TestSessionButton from './components/TestSessionButton';
+// <TestLoginButton aProp={{objKey: 'objValue'}} {...{propName: 'PropValue'}} />
+// <TestSessionButton/>
 
 class App extends Component {
-  handleClick = () => {
-    axios.post('/login', {
-      email: "abc",
-      password: "12"
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log('ran error catch.');
-      console.log('error :', error);
-    });
-  }
-
-  handleOtherClick = () => {
-    axios.get('/sessiontest')
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log('ran error catch.');
-      console.log('error :', error);
-    });
-
-}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.handleClick}>LOGIN</button>
-        <button onClick={this.handleOtherClick}>GetSessionTest</button>
+        <h1>App Component</h1>
       </div>
     );
   }
