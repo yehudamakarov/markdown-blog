b2fcce02a0ee1b2bc526785170c5da39df88b47d
github username <github email address>
2018-07-23 17:45:05 -0400

    great appbar. positioning and functionality perfect.

--- client/src/adminContainer/components/AppBar.jsx
+++ client/src/adminContainer/components/AppBar.jsx
@@ -1,51 +1,79 @@
-import Dashboard from '../components/new/Dashboard'
-import PublicContainer from '../../publicContainer/PublicContainer';
-import Logout from '../components/Logout'
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
-const styles = {
+import Dashboard from '../components/new/Dashboard';
+import PublicContainer from '../../publicContainer/PublicContainer';
+import Logout from '../components/Logout'
+import Toolbar from '@material-ui/core/Toolbar';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
   root: {
     flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
   },
   flex: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+});
 
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            News
-          </Typography>
-          <Logout />
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+              <Tabs className={classes.flex} value={value} onChange={this.handleChange}>
+                <Tab label="New" />
+                <Tab label="Edit" />
+              </Tabs>
+              <Logout />
+          </Toolbar>
+        </AppBar>
+        {value === 0 && 
+            <TabContainer>
+                <Dashboard />
+            </TabContainer>
+        }
+        {value === 1 && 
+            <TabContainer>
+                <PublicContainer />
+            </TabContainer>
+        }
+      </div>
+    );
+  }
 }
 
-ButtonAppBar.propTypes = {
+SimpleTabs.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ButtonAppBar);
+export default withStyles(styles)(SimpleTabs);
