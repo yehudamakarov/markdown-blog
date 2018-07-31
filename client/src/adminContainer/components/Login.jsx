551be08cfa4e4c1b176ca3dc93797d5cab8905f6
github username <github email address>
2018-07-31 12:49:53 -0400

    got eslint and prettier working.

--- client/src/adminContainer/components/Login.jsx
+++ client/src/adminContainer/components/Login.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { Redirect } from "react-router-dom";
-import { loginAction } from '../../store/actions/loginAction';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { loginAction } from '../../store/actions/loginAction';
 
 class Login extends Component {
     constructor(props) {
@@ -9,58 +9,51 @@ class Login extends Component {
         this.state = {
             email: '',
             password: '',
-        }
+        };
     }
+
     onChange = ({ target }) => {
-        this.setState(
-            {
-                [target.name]: target.value
-            }
-        )
-    }
-    onSubmit = (event) => {
+        this.setState({
+            [target.name]: target.value,
+        });
+    };
+
+    onSubmit = event => {
+        const { loginAction } = this.props;
         event.preventDefault();
-        this.props.loginAction(this.state)
-    }
+        loginAction(this.state);
+    };
+
     render() {
         const { email, password } = this.state;
         const { isLoggedIn } = this.props;
         if (isLoggedIn === true) {
-            return <Redirect to='/admin' />
+            return <Redirect to="/admin" />;
         }
         return (
-          <div>
-            <form onSubmit={this.onSubmit}>
-                <div>
-                    Email:
-                    <input
-                        type="text"
-                        value={email}
-                        name='email'
-                        onChange={this.onChange}
-                    />
-                </div>
-                <div>
-                    Password: 
-                    <input
-                        type="password"
-                        value={password}
-                        name='password'
-                        onChange={this.onChange}
-                    />
-                </div>
-                <div>
-                    <input type="submit"/>
-                </div>
-            </form>
-          </div>
-        )
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        Email:
+                        <input type="text" value={email} name="email" onChange={this.onChange} />
+                    </div>
+                    <div>
+                        Password:
+                        <input type="password" value={password} name="password" onChange={this.onChange} />
+                    </div>
+                    <div>
+                        <input type="submit" />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
-const mapStateToProps = (state) => {
-    return {
-      isLoggedIn: state.auth.isLoggedIn
-    }
-}
-  
-export default connect(mapStateToProps,{ loginAction })(Login)
+const mapStateToProps = state => ({
+    isLoggedIn: state.auth.isLoggedIn,
+});
+
+export default connect(
+    mapStateToProps,
+    { loginAction }
+)(Login);
--- client/src/publicContainer/Display.jsx
+++ client/src/publicContainer/Display.jsx
@@ -1,27 +1,9 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-export default class Display extends Component {
-    
-  render() {
-    const { match } = this.props;
-    return (
-      <div>
-        <Route path={`${match.url === '/' ? '' : match.url}/tags/:tag`} render={(routeProps) => 
-            <div>
-                <h1>Posts By Tag</h1>
-                <p>{routeProps.match.url}</p>
-                <p>{routeProps.match.path}</p>
-            </div>
-        } />
-        <Route exact path={`${match.url}`} render={(routeProps) => 
-            <div>
-                <h1>Post Index</h1>
-                <p>{routeProps.match.url}</p>
-                <p>{routeProps.match.path}</p>
-            </div>
-        } />
-      </div>
-    )
-  }
-}
+export default ({ match }) => (
+    <div>
+        <Route path={`${match.url === '/' ? '' : match.url}/tags/:tag`} />
+        <Route exact path={`${match.url}`} />
+    </div>
+);
