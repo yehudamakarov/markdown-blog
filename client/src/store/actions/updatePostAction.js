3be4350992e7a3fbabd59526126b649b99506533
github username <github email address>
2018-08-05 21:17:41 -0400

    filters right after post update

--- client/src/store/actions/updatePostAction.js
+++ client/src/store/actions/updatePostAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const updatePostAction = (postToUpdateFromState, id) => dispatch =>
-    axios.patch(`posts/${id}`, postToUpdateFromState).then(() => {
+    axios.patch(`/posts/${id}`, postToUpdateFromState).then(() => {
         axios.get('/posts').then(resp => {
             const posts = resp.data;
             dispatch({
