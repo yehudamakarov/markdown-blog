07736d7310716c1bb3fa6d8825bebfa6da0b4eaa
Yehuda Makarov <yehudamakarov@gmail.com>
2018-12-18 00:13:47 -0500

    turned off the devtools to prevent error

--- client/src/store/createStore.js
+++ client/src/store/createStore.js
@@ -2,17 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk), // eslint-disable-next-line no-underscore-dangle, no-undef
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
-
 // const store = createStore(
 //     rootReducer,
-//     applyMiddleware(thunk)
-// )
+//     compose(
+//         applyMiddleware(thunk), // eslint-disable-next-line no-underscore-dangle, no-undef
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// );
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
